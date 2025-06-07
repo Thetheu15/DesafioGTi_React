64eb02bfab2ea1acd965d699d5ea8d6b13317052
Thetheu15 <antoniomatheus0385@gmail.com>
2025-06-07 10:41:20 -0300

    Initial commit: Projeto React configurado

---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Pagina from "./pagina.jsx";
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <Pagina />
+);
