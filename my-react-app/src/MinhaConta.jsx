201e1ff2d5b296bd1d1d8a288e484c03f71148f1
Thetheu15 <149914121+Thetheu15@users.noreply.github.com>
2025-07-08 23:19:42 -0300

    Update MinhaConta.jsx

--- my-react-app/src/MinhaConta.jsx
+++ my-react-app/src/MinhaConta.jsx
@@ -20,7 +20,7 @@ function MinhaConta() {
 
     const navigate = useNavigate();
 
-    const BACKEND_URL = 'http://localhost:8080';
+    const BACKEND_URL = 'https://gameforge-backend.onrender.com';
     const USER_ME_ENDPOINT = '/users/me';
     const CHANGE_PASSWORD_ENDPOINT = '/users/me/password';
 
